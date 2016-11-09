4197aecbc7ce4ee80ab9b97c543ce1c574650ec9
ducthinhdt <ducthinhdt@gmail.com>
2016-11-09 20:55:52 +0700

    temporary fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,7 +81,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'iprestrict.middleware.IPRestrictMiddleware',
+    # 'iprestrict.middleware.IPRestrictMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
